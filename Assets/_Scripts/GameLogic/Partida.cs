8413413988c5ea64e2eb7ac1234ee0b1bb254554
Felipe Genú <fgenu@id.uff.br>
2018-10-30 03:56:18 -0300

    Escrito verificação de empate por afogamento e por insuficiência de material.

--- Assets/_Scripts/GameLogic/Partida.cs
+++ Assets/_Scripts/GameLogic/Partida.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using System.Linq;
 
 public class Partida
 {
@@ -9,11 +10,11 @@ public class Partida
 	public Tabuleiro Tabuleiro { get; private set; }
 	public Jogador Jogador1 { get; private set; }
 	public Jogador Jogador2 { get; private set; }
-    public IA jIA;
-    public UITabuleiro UItab;
-    public int Turno { get; private set; }
-    public int turnospassados=0; // acredito que isso vai ser importante mais tarde para implementar empate.
-    // depois deve criar uma função que incrementa essa variavel ou incrementar ela na função que troca os turnos (TODO)
+	public IA jIA;
+	public UITabuleiro UItab;
+	public int Turno { get; private set; }
+	public int turnospassados = 0; // acredito que isso vai ser importante mais tarde para implementar empate.
+								   // depois deve criar uma função que incrementa essa variavel ou incrementar ela na função que troca os turnos (TODO)
 
 
 	public Partida()
@@ -21,48 +22,50 @@ public class Partida
 		Tabuleiro = new Tabuleiro();
 		Jogador1 = new Jogador('b', false);
 		Jogador2 = new Jogador('p', true);
-        Tabuleiro.partida = this;
-        jIA = new IA(Jogador2);
+		Tabuleiro.partida = this;
+		jIA = new IA(Jogador2);
 		IniciarPartida(Jogador1, Jogador2);
 	}
 
 	// TODO: avaliar necessidade desta função
-    public int retornaTurno()
-    {
-        return Turno;
-    }
-    public void refazReferencias() {
-        foreach (Peca p in Jogador1.conjuntoPecas)
-        {
-            p.CasaAtual.PecaAtual = p;
-        }
+	public int retornaTurno()
+	{
+		return Turno;
+	}
+	public void refazReferencias()
+	{
+		foreach (Peca p in Jogador1.conjuntoPecas)
+		{
+			p.CasaAtual.PecaAtual = p;
+		}
 
 
-        foreach (Peca p in Jogador2.conjuntoPecas)
-        {
-            p.CasaAtual.PecaAtual = p;
-        }
-    }
+		foreach (Peca p in Jogador2.conjuntoPecas)
+		{
+			p.CasaAtual.PecaAtual = p;
+		}
+	}
 	public void PassarAVez()
-    {
-        VerificaVitoria();
+	{
+		VerificaEmpate();
+		VerificaVitoria();
 		if (Turno == 1)
-        {
-            Turno = 2;
-            Tabuleiro.PrintaTabuleiro();
-            Movimento m = jIA.minmax(3, -222222222,2222222222,true,Tabuleiro).movimento;
-            Debug.Log(m.origem.PosX+"   "+m.origem.PosY);
-            UItab.TryMove(m.origem.uiC, m.destino.uiC);
-            refazReferencias();
-            Tabuleiro.PrintaTabuleiro();
-        }
-        else
-        {
-            Turno = 1;
-        }
-        Debug.Log(Turno);
+		{
+			Turno = 2;
+			Tabuleiro.PrintaTabuleiro();
+			Movimento m = jIA.minmax(3, -222222222, 2222222222, true, Tabuleiro).movimento;
+			Debug.Log(m.origem.PosX + "   " + m.origem.PosY);
+			UItab.TryMove(m.origem.uiC, m.destino.uiC);
+			refazReferencias();
+			Tabuleiro.PrintaTabuleiro();
+		}
+		else
+		{
+			Turno = 1;
+		}
+		Debug.Log(Turno);
 
-    }
+	}
 
 	private Jogador JogadorDaVez()
 	{
@@ -79,37 +82,37 @@ public class Partida
 		// o rei não pode se movimentar para nenhuma casa sem que sofra um ataque(não há movimentos validos)
 		// a peça(s) que esta(m) atacando não podem ser capturadas pelo rei
 		//nenhuma peça pode proteger o rei (entrar na frente do atacante)
-		int numameacas=0;
-		int numprote=0;
+		int numameacas = 0;
+		int numprote = 0;
 		// O rei está em xeque?	
 		if (JogadorDaVez().inimigo.EmXeque())
 		{
 			foreach (Peca peca in JogadorDaVez().inimigo.conjuntoPecas) // loop para achar o rei no conjunto de pecas inimigas
 			{
-				
-				
-				if(peca is Rei)
+
+
+				if (peca is Rei)
 				{
 					// agora deve-se verificar se o rei inimigo possui algum movimento valido (ou seja nenhum movimento o deixa em xeque ainda)
-					if (peca.ListaMovimentos(Tabuleiro, peca.CasaAtual).Count > 0 )
+					if (peca.ListaMovimentos(Tabuleiro, peca.CasaAtual).Count > 0)
 					{
-			
+
 						return false;
 					}
 					//agora deve-se verificar se as peças que ameaçam o rei estão a mais de uma casa de distancia dele(nao podem ser capturadas pelo rei)
 					foreach (Peca pa in JogadorDaVez().conjuntoPecas)
 					{
-						if (pa.CasaAtual != null)  
-						{		
-							List<Movimento> movimentos = pa.ListaMovimentos(pa.CasaAtual.Tabuleiro, pa.CasaAtual, false); 
+						if (pa.CasaAtual != null)
+						{
+							List<Movimento> movimentos = pa.ListaMovimentos(pa.CasaAtual.Tabuleiro, pa.CasaAtual, false);
 							if (movimentos.Count > 0)
 							{
-								foreach (Movimento mov in movimentos)  
+								foreach (Movimento mov in movimentos)
 								{
 									if (mov.destino.PecaAtual is Rei && mov.tipo != Movimento.Tipo.SemCaptura) // se alguma das possibilidades de movimento envolve captura em direção ao rei
-									{							
-										
-										if( (peca.PosY == pa.PosY && Math.Abs(peca.PosX - pa.PosX) == 1 )|| (peca.PosX == pa.PosX && Math.Abs(peca.PosY - pa.PosY) == 1 ) || ( (Math.Abs(peca.PosX - pa.PosX) == 1) && ( Math.Abs(peca.PosY - pa.PosY) == 1 ) ) ) 
+									{
+
+										if ((peca.PosY == pa.PosY && Math.Abs(peca.PosX - pa.PosX) == 1) || (peca.PosX == pa.PosX && Math.Abs(peca.PosY - pa.PosY) == 1) || ((Math.Abs(peca.PosX - pa.PosX) == 1) && (Math.Abs(peca.PosY - pa.PosY) == 1)))
 										{
 											return false;
 										}
@@ -123,15 +126,15 @@ public class Partida
 					// (acho que deve ter alguma forma de deixar esse código melhor sem ter que chamar outro foreach aqui, mas depois eu faço isso) 
 					//TODO*
 					foreach (Peca prot in JogadorDaVez().inimigo.conjuntoPecas)
-					{	
-						if(!(prot is Rei))
+					{
+						if (!(prot is Rei))
 						{
-							if (prot.CasaAtual != null)  
+							if (prot.CasaAtual != null)
 							{
 								List<Movimento> movimentosprot = prot.ListaMovimentos(prot.CasaAtual.Tabuleiro, prot.CasaAtual, false);
-								foreach (Movimento mov in movimentosprot )
+								foreach (Movimento mov in movimentosprot)
 								{
-									if( (peca.PosY == mov.destino.PosY && Math.Abs(peca.PosX - mov.destino.PosX) == 1 )|| (peca.PosX == mov.destino.PosX && Math.Abs(peca.PosY - mov.destino.PosY) == 1 ) || ( (Math.Abs(peca.PosX - mov.destino.PosX) == 1) && ( Math.Abs(peca.PosY - mov.destino.PosY) == 1 ) )  )
+									if ((peca.PosY == mov.destino.PosY && Math.Abs(peca.PosX - mov.destino.PosX) == 1) || (peca.PosX == mov.destino.PosX && Math.Abs(peca.PosY - mov.destino.PosY) == 1) || ((Math.Abs(peca.PosX - mov.destino.PosX) == 1) && (Math.Abs(peca.PosY - mov.destino.PosY) == 1)))
 									{
 										numprote++;
 									}
@@ -139,7 +142,7 @@ public class Partida
 							}
 						}
 					}
-					if(numprote == numameacas)
+					if (numprote == numameacas)
 					{
 						return false;
 					}
@@ -150,38 +153,97 @@ public class Partida
 					break; // evitar percorrer o resto do conjunto de pecas atoa
 
 				}
-				
-				
+
+
 			}
 
-		
 
-		
+
+
 		}
 
 		return true;
 
-		
+
 	}
 
-	// definem o final do jogo (TODO) (deve verificar afogamento de rei, impossibilidade de xeque, regra dos 50 movimentos)
+	// definem o final do jogo (TODO: verificar mais impossibilidade de xeque, regra dos 50 movimentos)
 	public bool VerificaEmpate()
 	{
+		var pecasAliadas = JogadorDaVez().conjuntoPecas;
+		var pecasInimigas = JogadorDaVez().inimigo.conjuntoPecas;
+		
+		// Afogamento
+		bool semMovimentos = true;
+
+		foreach (Peca peca in pecasInimigas)
+		{
+			if (peca.ListaMovimentos(Tabuleiro, peca.CasaAtual).Count > 0)
+			{
+				semMovimentos = false;
+				break;
+			}
+		}
+
+		if (semMovimentos && !JogadorDaVez().inimigo.EmXeque())
+		{
+			Debug.Log("Empate! Rei afogado.");
+			return true;
+		}
+
+
+		// Impossibilidade de xeque por falta de peças (supõe que cada jogador possui um Rei)
+		if (pecasInimigas.Count == 1)
+		{
+			if (pecasAliadas.Count == 1) // Apenas Reis
+			{
+				Debug.Log("Empate! Xeque impossível.");
+				return true;
+			}
+
+			if (pecasAliadas.Count == 2)
+			{
+				foreach (var peca in pecasAliadas)
+				{
+					if (peca is Bispo || peca is Cavalo) // Apenas Rei e Bispo ou Rei e Cavalo
+					{
+						Debug.Log("Empate! Xeque impossível.");
+						return true;
+					}
+				}
+			}
+		}
+		else if (pecasInimigas.Count == 2 && pecasAliadas.Count == 2)
+		{
+			foreach (Bispo bispo in pecasAliadas)
+			{
+				foreach (Bispo bispoInimigo in pecasInimigas)
+				{
+					if (bispo.CasaAtual.cor == bispoInimigo.CasaAtual.cor) // Apenas Rei e Bispo dos dois lados, bispos em casa de mesma cor
+					{
+						Debug.Log("Empate! Xeque impossível.");
+						return true;
+					}	
+				}
+			}
+		}
+
+
 		return false;
-	} 
+	}
 
 	void IniciarPartida(Jogador j1, Jogador j2)
 	{
 		j1.inimigo = j2;
 		j2.inimigo = j1;
-        if (j1.Cor == 'b')
-        {
-            Turno = 1;
-        }
-        else
-        {
-            Turno = 2;
-        }
+		if (j1.Cor == 'b')
+		{
+			Turno = 1;
+		}
+		else
+		{
+			Turno = 2;
+		}
 		Tabuleiro.InserePecasNaPosicaoInicial(this);
 		Tabuleiro.PrintaTabuleiro();
 	}
